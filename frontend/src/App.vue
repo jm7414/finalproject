75b4aa96dacd482022039707f5019a792ad19d7a
ks07235 <amg07235@gmail.com>
2025-09-25 10:42:25 +0900

    프로젝트 시작

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 </script>
 
 <style scoped>
---
+++ frontend/src/views/Post.vue
@@ -0,0 +1,24 @@
+
+<template>
+
+  <body style="">
+
+    <div class="card" style="width: 18rem;">
+      <img src="" class="card-img-top" alt="">
+      <div class="card-body">
+        <p class="card-text">실종자 이름 / 실종 시간</p>
+      </div>
+    </div>
+
+  </body>
+
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
