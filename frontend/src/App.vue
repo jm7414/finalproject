047150b36262af52f5416cb9732649c910f766c4
jm7414 <88954941+jm7414@users.noreply.github.com>
2025-09-25 15:03:35 +0900

    카카오 맵 api 작업중 커밋(아직 미완료에요)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <style scoped>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,15 @@ import router from './router'
 
 const app = createApp(App)
 
+// 카카오 api 연결용
+import { createApp } from 'vue'
+import App from './App.vue'
+import { useKakao } from 'vue3-kakao-maps/@utils'
+
+useKakao('110dada163e51c3af92f7eebc8de2440')
+
+createApp(App).mount('#app')
+
 app.use(createPinia())
 app.use(router)
 
