326f0076e44758c06329742b39d10d3fa5f820b0
jm7414 <88954941+jm7414@users.noreply.github.com>
2025-09-26 15:22:49 +0900

    캘린더 작업 전 임시 커밋, 크게 변경 사항 없는 ver

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="home">
-    <h1>This is an home page</h1>
-  </div>
+  <div ref="mapContainer" class="map-box"></div>
+  <div class="row g-3 mt-4"></div>
+
+
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 
+const mapContainer = ref(null)
+
+/* 카카오 지도 (그대로) */
+const KAKAO_JS_KEY = '52b0ab3fbb35c5b7adc31c9772065891'
+onMounted(() => {
+  loadKakaoMap(mapContainer.value)
+})
+function loadKakaoMap(container) {
+  const script = document.createElement('script')
+  // 현재 service, cluster, drawing 라이브러리를 모두 활성화 시킴
+  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_JS_KEY}&libraries=services,clusterer,drawing&autoload=false`
+  document.head.appendChild(script)
+  script.onload = () => {
+    window.kakao.maps.load(() => {
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 4,
+        maxLevel: 5,
+      }
+      const mapInstance = new window.kakao.maps.Map(container, options)
+      // 지도 중앙에 마커 생성
+      const marker = new kakao.maps.Marker({
+        position: mapInstance.getCenter(),
+        map: mapInstance,
+      })
+    })
+  }
+}
 </script>
 
 <style scoped>
-
+.map-box {
+  width: 100%;
+  height: 700px;
+}
 </style>
