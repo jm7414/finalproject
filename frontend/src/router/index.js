684604c00a3af6d665822d96e99cea3da1db42b6
JinJuKing <bammay23@gmail.com>
2025-09-30 11:25:28 +0900

    로그인 회원가입했슴다

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,8 @@ import DP_main from '@/views/DP_main.vue'
 import GD_main from '@/views/GD_main.vue'
 import GeoFencingView from '@/views/GeoFencingView.vue'
 import SearchRouteView from '@/views/SearchRouteView.vue'
-import Signup from '@/views/Signup.vue'
+import Signup from '@/views/SignUp.vue'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +35,11 @@ const router = createRouter({
       path: '/SignUp',
       name: 'SignUp',
       component: Signup,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
     {
       path: '/CommunityView',
