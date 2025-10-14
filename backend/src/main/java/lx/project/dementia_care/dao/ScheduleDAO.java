c84f1e18aba5becd23b1d4584ff57f9f4412eaab
jm7414 <88954941+jm7414@users.noreply.github.com>
2025-10-14 16:22:35 +0900

    ScheduleDAO 코드 형식 단일화 작업

--- backend/src/main/java/lx/project/dementia_care/dao/ScheduleDAO.java
+++ backend/src/main/java/lx/project/dementia_care/dao/ScheduleDAO.java
@@ -21,42 +21,42 @@ public class ScheduleDAO {
 
     // 일정 저장
     public int insertSchedule(ScheduleVO schedule) throws Exception {
-        return session.insert("lx.project.dementia_care.dao.ScheduleDAO.insertSchedule", schedule);
+        return session.insert("mapper-schedule.insertSchedule", schedule);
     }
 
     // 일정 위치 저장
     public int insertScheduleLocation(ScheduleLocationVO location) throws Exception {
-        return session.insert("lx.project.dementia_care.dao.ScheduleDAO.insertScheduleLocation", location);
+        return session.insert("mapper-schedule.insertScheduleLocation", location);
     }
 
     // 경로 저장
     public int insertRoute(RouteVO route) throws Exception {
-        return session.insert("lx.project.dementia_care.dao.ScheduleDAO.insertRoute", route);
+        return session.insert("mapper-schedule.insertRoute", route);
     }
 
     // 안심존 저장
     public int insertSafeZone(SafeZoneVO safeZone) throws Exception {
-        return session.insert("lx.project.dementia_care.dao.ScheduleDAO.insertSafeZone", safeZone);
+        return session.insert("mapper-schedule.insertSafeZone", safeZone);
     }
 
     // 특정 사용자의 모든 일정 조회
     public List<ScheduleVO> getSchedulesByUserNo(int userNo) throws Exception {
-        return session.selectList("lx.project.dementia_care.dao.ScheduleDAO.getSchedulesByUserNo", userNo);
+        return session.selectList("mapper-schedule.getSchedulesByUserNo", userNo);
     }
 
     // 특정 일정의 위치 목록 조회
     public List<ScheduleLocationVO> getLocationsByScheduleNo(int scheduleNo) throws Exception {
-        return session.selectList("lx.project.dementia_care.dao.ScheduleDAO.getLocationsByScheduleNo", scheduleNo);
+        return session.selectList("mapper-schedule.getLocationsByScheduleNo", scheduleNo);
     }
 
     // 특정 일정의 경로 조회
     public RouteVO getRouteByScheduleNo(int scheduleNo) throws Exception {
-        return session.selectOne("lx.project.dementia_care.dao.ScheduleDAO.getRouteByScheduleNo", scheduleNo);
+        return session.selectOne("mapper-schedule.getRouteByScheduleNo", scheduleNo);
     }
 
     // 특정 사용자의 모든 안심존 조회
     public List<SafeZoneVO> getSafeZonesByUserNo(int userNo) throws Exception {
-        return session.selectList("lx.project.dementia_care.dao.ScheduleDAO.getSafeZonesByUserNo", userNo);
+        return session.selectList("mapper-schedule.getSafeZonesByUserNo", userNo);
     }
 
     // 특정 사용자의 특정 타입 안심존 조회
@@ -64,47 +64,47 @@ public class ScheduleDAO {
         Map<String, Object> params = new HashMap<>();
         params.put("userNo", userNo);
         params.put("zoneType", zoneType);
-        return session.selectList("lx.project.dementia_care.dao.ScheduleDAO.getSafeZonesByUserNoAndType", params);
+        return session.selectList("mapper-schedule.getSafeZonesByUserNoAndType", params);
     }
 
     // 현재 시간에 해당하는 일정 조회
     public ScheduleVO getCurrentSchedule(int userNo) throws Exception {
-        return session.selectOne("lx.project.dementia_care.dao.ScheduleDAO.getCurrentSchedule", userNo);
+        return session.selectOne("mapper-schedule.getCurrentSchedule", userNo);
     }
 
     // 특정 일정 조회 (by scheduleNo)
     public ScheduleVO getScheduleByNo(int scheduleNo) throws Exception {
-        return session.selectOne("lx.project.dementia_care.dao.ScheduleDAO.getScheduleByNo", scheduleNo);
+        return session.selectOne("mapper-schedule.getScheduleByNo", scheduleNo);
     }
 
     // 일정 수정
     public int updateSchedule(ScheduleVO schedule) throws Exception {
-        return session.update("lx.project.dementia_care.dao.ScheduleDAO.updateSchedule", schedule);
+        return session.update("mapper-schedule.updateSchedule", schedule);
     }
 
     // 일정 삭제
     public int deleteSchedule(int scheduleNo) throws Exception {
-        return session.delete("lx.project.dementia_care.dao.ScheduleDAO.deleteSchedule", scheduleNo);
+        return session.delete("mapper-schedule.deleteSchedule", scheduleNo);
     }
 
     // 특정 일정의 위치 삭제
     public int deleteScheduleLocations(int scheduleNo) throws Exception {
-        return session.delete("lx.project.dementia_care.dao.ScheduleDAO.deleteScheduleLocations", scheduleNo);
+        return session.delete("mapper-schedule.deleteScheduleLocations", scheduleNo);
     }
 
     // 특정 일정의 경로 삭제
     public int deleteRoute(int scheduleNo) throws Exception {
-        return session.delete("lx.project.dementia_care.dao.ScheduleDAO.deleteRoute", scheduleNo);
+        return session.delete("mapper-schedule.deleteRoute", scheduleNo);
     }
 
     // 특정 사용자의 경로형 안심존 삭제 (일정 삭제 시)
     public int deleteSafeZoneByUserNo(int userNo) throws Exception {
-        return session.delete("lx.project.dementia_care.dao.ScheduleDAO.deleteSafeZoneByUserNo", userNo);
+        return session.delete("mapper-schedule.deleteSafeZoneByUserNo", userNo);
     }
 
     // 안심존 수정
     public int updateSafeZone(SafeZoneVO safeZone) throws Exception {
-        return session.update("lx.project.dementia_care.dao.ScheduleDAO.updateSafeZone", safeZone);
+        return session.update("mapper-schedule.updateSafeZone", safeZone);
     }
 }
 
